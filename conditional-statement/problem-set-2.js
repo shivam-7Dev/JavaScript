c6ca446dae8516dc81b5f8d831a9f313132e4d3d
Shivam Sharma <shivam7.dev@gmail.com>
2024-04-08 16:12:03 +0530

    coditional statment exercise

---
+++ conditional-statement/problem-set-2.js
@@ -0,0 +1,16 @@
+/*
+    Create a program that starts with a number variable.
+    The program will print out whether the variable is
+    even or odd to the screen when run.
+
+    Change the variable to test it works in both cases.
+
+    HINT: modulus operator ( % ) is your friend
+*/
+
+const number = 20;
+if (number % 2 === 0) {
+  console.log("Even");
+} else {
+  console.log("Odd");
+}
---
+++ conditional-statement/problem-set-3.js
@@ -0,0 +1,32 @@
+/* 
+   Spot the bug 🐞
+   Should print:
+   "Most places consider you an adult."
+   but it doesn't, why?
+*/
+const age = 20;
+
+if (age < 3) {
+  console.log("You're just a baby!");
+} else if (age >= 3) {
+  console.log("You are in elementary school, kid.");
+} else if (age >= 13) {
+  console.log("Nice, you're a teenager!");
+} else if (age >= 18) {
+  console.log("Most places consider you an adult.");
+} else {
+  console.log("What? How did this happen!?");
+}
+
+//correct version
+if (age < 3) {
+  console.log("You're just a baby!");
+} else if (age >= 18) {
+  console.log("Most places consider you an adult.");
+} else if (age >= 3) {
+  console.log("You are in elementary school, kid.");
+} else if (age >= 13) {
+  console.log("Nice, you're a teenager!");
+} else {
+  console.log("What? How did this happen!?");
+}
