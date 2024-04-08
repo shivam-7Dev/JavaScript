e6bdd0297d2d4851c3a133c87f47cbb4291a994a
Shivam Sharma <shivam7.dev@gmail.com>
2024-04-08 08:57:06 +0530

    added prmetives

---
+++ variables-primitives/problem-set-warmup.js
@@ -0,0 +1,26 @@
+/* 
+   Create a constant variable of each of the following primitive types:
+   - String
+   - Number
+   - Boolean
+   - Null
+   - Undefined
+
+   Use console.log to log out each variable to the terminal at the end.
+
+   HINT1: You can run your file in the terminal using this command: node <file_name>
+          For example, if your file is called ps1.js, you would type: node ps1.js
+
+   HINT2: Make sure your terminal is "looking at" the correct folder where your code is
+*/
+const myString = "Hello";
+const myNumber = 42;
+const myBoolean = true;
+const myNull = null;
+const myUndefined = undefined;
+
+console.log(myString);
+console.log(myNumber);
+console.log(myBoolean);
+console.log(myNull);
+console.log(myUndefined);
