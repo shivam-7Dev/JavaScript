98411a87fee3e96166d593fb23de088237d8b269
Shivam Sharma <shivam7.dev@gmail.com>
2024-04-17 23:45:19 +0530

    added solution to npm  exercise 3 solution

--- npm/exercise3/hello.js
+++ npm/exercise3/hello.js
@@ -1,1 +1,11 @@
-console.log("test");
+import { Command, program } from "commander";
+// const program = new Command();
+
+// for (const key in program) {
+//   console.log(key);
+// }
+
+program.option("-n, --name <name>", "Your name").parse(process.argv);
+const options = program.opts();
+
+console.log(`Hello, ${options.name}!`);
